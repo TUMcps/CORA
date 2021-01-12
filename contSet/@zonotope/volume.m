5c1e767a8bffc8e9717f90093cb6f98c29768885
Mark Wetzlinger <m.wetzlinger@tum.de>
2021-01-12 07:43:20 +0100

    contSet/zonotope: clarification of approximative calculation of volume

--- contSet/@zonotope/volume.m
+++ contSet/@zonotope/volume.m
@@ -1,4 +1,4 @@
-function vol = volume(Z,r)
+function vol = volume(Z,varargin)
 % volume - Computes the volume of a zonotope according to page 40 in [1]
 %
 % Syntax:  
@@ -6,14 +6,15 @@ function vol = volume(Z,r)
 %
 % Inputs:
 %    Z - zonotope object
-%    r - ? (for approximative calculation)
+%    r - number of filtered generators in addition to minimal requirement
+%           (full rank) for approximative calculation
 %
 % Outputs:
 %    vol - volume
 %
 % Example: 
-%    Z=zonotope([1 -1 0; 0 0 -1]);
-%    vol=volume(Z)
+%    Z = zonotope([1 -1 0; 0 0 -1]);
+%    vol = volume(Z)
 %
 % References:
 %    [1] E. Grover et al. "Determinants and the volumes of parallelotopes 
@@ -31,12 +32,20 @@ function vol = volume(Z,r)
 %               ??-May-2016 (AK, accumulator)
 %               02-Sep-2019 (incl. approx)
 %               04-May-2020 (MW, add vol=0 cases)
+%               12-January-2021 (MW, clarification of approx case)
 % Last revision:---
 
 %------------- BEGIN CODE --------------
 
-if nargin < 2
-    r = false;
+r = [];
+approx = false;
+if nargin == 2
+    approx = true;
+    if isscalar(varargin{1}) && varargin{1} >= 0 && abs(mod(varargin{1},1)) < eps
+        r = varargin{1};
+    else
+        error("Number of filtered generators invalid!");
+    end
 end
 
 %dimension and nrOfGenerators
@@ -46,9 +55,13 @@ G=generators(Z);
 if nrOfGens < n || rank(G) < n
     vol = 0;
     return
+elseif ~isempty(r) && r > nrOfGens - n
+    warning(sprintf(['Number of filtered generators matches/exceeds size of generator matrix!\n',...
+        '--> default method ''exact computation'' selected']));
+    approx = false; r = [];
 end
 
-if ~r
+if ~approx
     % exact calculation
 
     %possible combinations of n=dim generators from all generators
@@ -67,7 +80,7 @@ if ~r
         end
     end
 
-    vol=2^n*accVol;
+    vol = 2^n*accVol;
 
 else
     % approximative calculation
@@ -79,22 +92,22 @@ else
     % for i=1:nrOfGens
     %     h(i)=norm(G(:,i)'*G,1);
     % end
-    [~,indexFiltered]=sort(h);
-    Gfiltered=G(:,indexFiltered((end-n-r+1):end));
+    [~,indexFiltered] = sort(h);
+    Gfiltered = G(:,indexFiltered((end-n-r+1):end));
 
     %dimension and new nrOfGenerators
-    [n,nrOfGens]=size(Gfiltered);
+    [n,nrOfGens] = size(Gfiltered);
 
     %possible combinations of n=dim generators from all generators
     comb = combinator(nrOfGens,n,'c');
-    nrOfComb=length(comb(:,1));
+    nrOfComb = length(comb(:,1));
 
     parallelogramVol = zeros(nrOfComb,1);
     for i=1:nrOfComb
-        parallelogramVol(i)=abs(det(Gfiltered(:,comb(i,:))));
+        parallelogramVol(i) = abs(det(Gfiltered(:,comb(i,:))));
     end
 
-    vol=2^n*sum(parallelogramVol);
+    vol = 2^n*sum(parallelogramVol);
     
 end
 
