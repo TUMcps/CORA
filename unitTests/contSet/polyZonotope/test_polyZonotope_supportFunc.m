902ba2d39e0a5e1d77f7b34caec37ab8be352a44
Tobias Ladner <tobias.ladner@tum.de>
2022-12-07 14:49:32 +0100

    CORA v2022.0.2
    - polyZonotope/supportFunc considers splitted sets with method 'split'

--- unitTests/contSet/polyZonotope/test_polyZonotope_supportFunc.m
+++ unitTests/contSet/polyZonotope/test_polyZonotope_supportFunc.m
@@ -19,9 +19,9 @@ function res = test_polyZonotope_supportFunc
 %
 % See also: -
 
-% Author:       Niklas Kochdumper
+% Author:       Niklas Kochdumper, Tobias Ladner
 % Written:      30-July-2018
-% Last update:  ---
+% Last update:  06-December-2022 (TL: more tests)
 % Last revision:---
 
 %------------- BEGIN CODE --------------
@@ -45,16 +45,55 @@ inter = interval(pZ,'bnb');
 % define ground truth
 inter_ = interval([0;1],[6;7]);
 
-% empty set
-pZ_e = polyZonotope();
-if supportFunc(pZ_e,[1;1],'upper') ~= -Inf || supportFunc(pZ_e,[1;1],'lower') ~= +Inf
-    res = false;
-end
-
 % check for correctness
 if ~isequal(inter,inter_)
     res = false;
+    return;
 end
+
+% test empty set
+pZ_e = polyZonotope();
+if supportFunc(pZ_e,[1;1],'upper') ~= -Inf || supportFunc(pZ_e,[1;1],'lower') ~= +Inf
+    res = false;
+    return;
+end
+
+% test [lower, upper] = range
+dir = [1 1];
+range = supportFunc(pZ, dir, 'range');
+lower = supportFunc(pZ, dir, 'lower');
+upper = supportFunc(pZ, dir, 'upper');
+
+if ~isequal(range, interval(lower, upper))
+    res = false;
+    return;
+end
+
+
+% TEST 2 (check point containment)
+
+c = [0;-1];
+G = [-0.5 0.4 0.04 -0.04; 0.6 1.9 1.7 -0.2];
+Grest = [];
+E = [0 4 2 1; 1 0 3 2];
+pZ = polyZonotope(c, G, Grest, E);
+
+% slightly under-approximative
+ground_truth = interval([-0.5059;-2.1168], [0.9; 3.34]);
+
+methods = ["interval", 'split', 'bnb', 'bnbAdv'];
+for method = methods
+    I = cartProd( ...
+      supportFunc(pZ, [1 0], 'range', method), ...
+      supportFunc(pZ, [0 1], 'range', method) ...
+    );
+
+    if ~I.contains(ground_truth, 'exact', 1e-15)
+        res = false;
+        return;
+    end
+end
+
 
 
 %------------- END OF CODE --------------
