8e71a6d630ad38b89c40b48e5f95ff563825faa4
Tobias Ladner <tobias.ladner@tum.de>
2023-07-18 15:54:47 +0200

    init

---
+++ matlab/convertToTikz.m
@@ -0,0 +1,40 @@
+function convertToTikz(figpath,varargin)
+
+% open figure
+openfig(figpath);
+box on;
+drawnow;
+
+% make tikz image
+tikzpath = strrep(figpath, '.fig', '.tikz');
+matlab2tikz(tikzpath, 'relativePrecision', 1e-4, ...
+    'floatFormatX', '%.4f', 'floatFormatY', '%.4f', ...
+    'showInfo',false,varargin{:});
+aux_postTikz(tikzpath);
+
+% close figure;
+% close;
+
+end
+
+
+% Auxiliary functions -----------------------------------------------------
+
+function aux_postTikz(tikzpath)
+
+fid  = fopen(tikzpath,'r');
+f=fread(fid,'*char')';
+fclose(fid);
+
+f = regexprep(f,'width=[0-9]*.[0-9]*in','width=8cm');
+f = regexprep(f,'height=[0-9]*.[0-9]*in','height=4.5cm');
+f = regexprep(f,'at=\{\([0-9]*.[0-9]*in,[0-9]*.[0-9]*in\)\},','at={(0in,0in)},');
+f = strrep(f,"\begin{tikzpicture}",compose("\\begin{tikzpicture}\n\\footnotesize"));
+% f = strrep(f,"legend style={","legend style={font=\tiny,");
+f = strrep(f,"mark size=0.5000pt","mark size=0.1000pt");
+
+fid  = fopen(tikzpath,'w');
+fprintf(fid,'%s',f);
+fclose(fid);
+
+end
