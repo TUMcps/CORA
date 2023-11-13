7524b2b342da67cce9f9918d8002cec16fa27306
Tobias Ladner <tobias.ladner@tum.de>
2023-11-13 11:10:34 +0100

    website: fixed publications

--- scripts/addPublication.js
+++ scripts/addPublication.js
@@ -1,11 +1,33 @@
-function addPublicationFromLink(listId, link) {
+function addPublicationsFromLink(listId, pubs) {
+  // {bibtex: "bibtex-link", type: "pub-type"}
+
+  // call the function to add the publication to the list
+  for (let i = 0; i < pubs.length; i++){
+    const pub = pubs[i];
+    const bibtexlink = "../../data/bib/" + pub.bibtex;
+    if (pub.type === 'diss') {
+      addDissFromLink("pubsUsingCORA", bibtexlink);
+    } else {
+      addPaperFromLink("pubsUsingCORA", bibtexlink);
+    }
+  }
+}
+
+function addPaperFromLink(listId, link) {
+  addPublicationFromLink(listId,link,"bi-file-text");
+}
+
+function addDissFromLink(listId, link) {
+  addPublicationFromLink(listId,link,"bi-journals");
+}
+
+function addPublicationFromLink(listId, link, icon) {
   // get the unordered list element by id
   var list = document.getElementById(listId);
   // create a new list item element
   var item = document.createElement("li");
 
   // use the fetch API to get the bibtex entry from the link
-  console.log(link);
   fetch(link)
   .then(response => response.text())
   .then(bibtex => {
@@ -13,28 +35,40 @@ function addPublicationFromLink(listId, link) {
     // use the bibtex-parser-js library to parse the bibtex entry
     //var parser = require('bibtex-parser-js');
     //var parsed = parser.toJSON(bibtex);
-    console.log(bibtex);
     const parsed = BibtexParser.parseToJSON(bibtex);
-    console.log(parsed);
     // get the first entry from the parsed object
     var entry = parsed[0];
     // create a string to store the publication details
-    var text = "";
+    // start with icon
+    var text = "<span class=\"timeline-icon\"><i class=\"bi " + icon + "\"></i></span>";
 
     // check if the entry has a title
     if (entry.title) {
       // add the title in bold
-      text += "<b>" + entry.title + "</b>";
+      text += "<h5>" + entry.title + "</h5>";
     }
+
+
+    // add date
+    date = "";
+    if (entry.month) {
+        date += entry.month;
+    }
+    if (entry.year) {
+      date += " " + entry.year;
+    }
+    text += "<p class=\"text-muted mb-2 fw-bold\"><i class=\"bi bi-calendar-event\"></i> " + date + "</p>";
 
     // check if the entry has authors
     if (entry.author) {
-      // add a comma after the title if it exists
-      if (text.length > 0) {
-        text += ", ";
-      }
+
       // add the authors separated by commas
-      text += entry.author.join(", ");
+      author = entry.author;
+      author = author.replace('\\\\\"a','&auml;')
+      author = author.replace('\\\\\"o','&ouml;')
+      author = author.replace('\\\\\"u','&uuml;')
+
+      text += author;
     }
 
     // check if the entry has a journal
@@ -101,6 +135,7 @@ function addPublicationFromLink(listId, link) {
     
     // set the inner HTML of the list item to the text
     item.innerHTML = text;
+    item.setAttribute('class','timeline-item mb-5');
     // append the list item to the list
     list.appendChild(item);
   })
@@ -109,3 +144,4 @@ function addPublicationFromLink(listId, link) {
     console.error(error);
     });
   }
+
