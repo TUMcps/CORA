099bc3c8c96675b21ccb14aeb96e59a8275a4677
Suhas <139655090+suhaslord@users.noreply.github.com>
2026-08-13 08:18:02 -0700

    test(zonotope): cover degenerate enclosePoints clouds

--- unitTests/contSet/zonotope/test_zonotope_enclosePoints.m
+++ unitTests/contSet/zonotope/test_zonotope_enclosePoints.m
@@ -22,7 +22,7 @@ function res = test_zonotope_enclosePoints
 % Last revision: ---
 
 % ------------------------------ BEGIN CODE -------------------------------
- 
+
 % assume true
 res = true;
 
@@ -43,5 +43,16 @@ Z_ = zonotope.enclosePoints(p,'stursberg');
 % check if all points are contained
 assert(all(contains(Z,p)))
 assert(all(contains(Z_,p)))
+
+% check degenerate and sign-cancelling point clouds
+pDegenerate = repmat([1; -2],1,4);
+ZDegenerate = zonotope.enclosePoints(pDegenerate);
+assert(all(isfinite([ZDegenerate.c; ZDegenerate.G(:)])))
+assert(all(contains(ZDegenerate,pDegenerate)))
+
+pCancellation = [1 -1; -1 1];
+ZCancellation = zonotope.enclosePoints(pCancellation);
+assert(all(isfinite([ZCancellation.c; ZCancellation.G(:)])))
+assert(all(contains(ZCancellation,pCancellation)))
 
 % ------------------------------ END OF CODE ------------------------------
